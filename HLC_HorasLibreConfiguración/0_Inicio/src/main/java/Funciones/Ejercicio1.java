96561325c670fdfe48768866e6ccd3202238c40a
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2024-09-20 16:39:52 +0200

    [HCL] 0-Incicio - Prueba de inicio

---
+++ HLC_HorasLibreConfiguración/0_Inicio/src/main/java/Funciones/Ejercicio1.java
@@ -0,0 +1,13 @@
+package Funciones;
+
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HOLA MUNDO DE LA CONSOLA");
+		System.err.println("HOLA MUNDO DE LOS ERRORES");
+
+	}
+
+}
