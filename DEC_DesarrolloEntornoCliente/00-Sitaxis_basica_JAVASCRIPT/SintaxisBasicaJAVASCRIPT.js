c337b248efaa2a389ba958237d9f8d7b585719b4
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2024-09-25 20:04:36 +0200

    [DEC] Primeros pasos en js

---
+++ DEC_DesarrolloEntornoCliente/00-Sitaxis_basica_JAVASCRIPT/SintaxisBasicaJAVASCRIPT.js
@@ -0,0 +1,22 @@
+Main();
+
+function Main() {
+    ambitosVariables();
+    let coche1={color: "rojo", marca: "Ford", modelo: "Mustang"};//? objeto coche
+    coche.color;
+    let coche2 = coche1;//? objeto coche con dos asignaciones, a la misma referencia
+    coche = null;
+    let letrasDNI=["t","r","w","a","g","m","y","f","p","d","x","b","n","j","z","s","q","v","h","l","c","k","e"];
+    letrasDNI.leng
+
+}
+
+
+
+function ambitosVariables() {
+
+    for (let index = 0; index < 10; index++) {
+        console.log("index= " + index);
+    }
+    console.log("variable index: " + index);
+}
