0bd33fcabe56638a4ef4592b0a1a0f7b83713fad
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2024-10-04 18:31:50 +0200

    [HLC] TODO Juego de aventura con mapa aleatorio

---
+++ HLC_HorasLibreConfiguración/Proyecto/src/Inicio/Test.java
@@ -0,0 +1,26 @@
+/**
+ * @author Pablo Gómez Meléndez
+ */
+package Inicio;
+
+public class Test {
+	/**
+	 * Se realizaran todas las pruebas 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		test();
+
+	}
+
+	private static void test() {
+		test1();
+		
+	}
+
+	private static void test1() {
+		
+		
+	}
+
+}
---
+++ HLC_HorasLibreConfiguración/Proyecto/src/Menus/Menu.java
@@ -0,0 +1,27 @@
+package Menus;
+
+import java.util.Scanner;
+
+public class Menu {
+	private Scanner teclado; 
+	public int menuInicio() {
+		teclado= new Scanner(System.in);
+		int valorMenu = 0;
+		String respuesta;
+		System.out.println("Bienvenido a esta aventura");
+		System.out.println("Estas listo para iniciar la aventura:[Si/No]");
+		respuesta=teclado.nextLine();
+		
+		
+		teclado.close();
+		return valorMenu;
+	}
+	public void menuPassword() {
+		teclado= new Scanner(System.in);
+		int valorMenu = 0;
+		System.out.println("Bienvenido a ");
+		
+		teclado.close();
+	}
+
+}
