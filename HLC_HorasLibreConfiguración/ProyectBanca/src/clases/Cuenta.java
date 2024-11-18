9d19589609e0849e6d15a522847529eb8542f810
Pablo Gómez Meléndez <pablogomezmelendez@gmail.com>
2024-11-18 19:57:20 +0100

    [HLC] Proyecto

--- HLC_HorasLibreConfiguración/ProyectBanca/src/clases/Cuenta.java
+++ HLC_HorasLibreConfiguración/ProyectBanca/src/clases/Cuenta.java
@@ -1,5 +1,84 @@
 package clases;
 
+import general.Contantes;
+
 public class Cuenta {
+	//Atributos
+	private double saldoCuenta;
+	private double extimacionDeGatos;
+	private boolean saldoPositivo;
+	
+	//Constructores
+	Cuenta(){
+		this.saldoCuenta=0;
+		this.saldoPositivo=Boolean.TRUE;
+	}
+	public Cuenta(double saldaCuenta) {
+		super();
+		this.saldoCuenta = saldaCuenta;
+		this.saldoPositivo=Boolean.TRUE;
+	}
+	
+	//Getters y Setters
+	public double getSaldoCuenta() {
+		return saldoCuenta;
+	}
+	public void setSaldoCuenta(double saldoCuenta) {
+		this.saldoCuenta = saldoCuenta;
+	}
+	public double getExtimacionDeGatos() {
+		return extimacionDeGatos;
+	}
+	public void setExtimacionDeGatos(double extimacionDeGatos) {
+		this.extimacionDeGatos = extimacionDeGatos;
+	}
+	public boolean isSaldoPositivo() {
+		return saldoPositivo;
+	}
+	public void setSaldoPositivo(boolean saldoPositivo) {
+		this.saldoPositivo = saldoPositivo;
+	}
+	//To String
+	@Override
+	public String toString() {
+		return "Cuenta [saldoCuenta=" + saldoCuenta + ", extimacionDeGatos=" + extimacionDeGatos + ", saldoPositivo="
+				+ saldoPositivo + "]";
+	}
+	
+	//Metodos
+	public void ingresarDineroEnCuenta(double ingreso) {
+		if (ingreso>=0) {			
+			setSaldoCuenta(getSaldoCuenta()+ingreso);
+		}else {
+			System.out.println(Contantes.INGRESO_DE_DINERO_EN_CUENTA);
+		}
+	}
+	public void gastarDinero(double gasto) {
+		if (isSaldoPositivo()) {
+			double aux= getSaldoCuenta()-gasto;
+			if(aux>=0) {
+				setSaldoCuenta(aux);
+			}else {
+				System.out.println(Contantes.CUENTA_ERROR_GASTO_SUPERIOR_A_SALDO);
+			}
+		}else {
+			System.out.println(Contantes.CUENTA_ERROR_GASTO_NO_POSITIVO);
+		}
+	}
+	public void retirarDinero(double retiro) {
+		if(isSaldoPositivo()) {
+			gastarDinero(retiro);
+		}else {
+			System.out.println(Contantes.CUENTA_ERROR_RETIRO_NO_POSITIVO);
+		}
+	}
+	public void pasarDinero(double cantidad, Cuenta cuenta) {
+		if(isSaldoPositivo()) {
+
+		}else {
+			System.out.println(Contantes.CUENTA_ERROR_PASAR_NO_POSITIVO);
+		}
+		
+	}
 
 }
--- HLC_HorasLibreConfiguración/ProyectBanca/src/general/Contantes.java
+++ HLC_HorasLibreConfiguración/ProyectBanca/src/general/Contantes.java
@@ -4,15 +4,27 @@ public class Contantes {
 	
 	public static final String SEPARADOR = "**********************************************************************************************************************************************************";
 
+	//TEXTOS LOGIN
 	public static final String LOGIN_USUARIO = "IDENTIFICATE CON EL NOMBRE DE USUARIO: ";
 	public static final String LOGIN_PASSWORD = "INTRODUCE LA CONTRASEÑA: ";
 	public static final String LOGIN_ERROR = "Usuario o contraseña erroneos";
 	public static final String LOGIN_EXITOSO_ADMIN = "Tuvo exito en registro como administrador. ";
 	public static final String LOGIN_EXITOSO_USER = "Tuvo exito en registro ";
+	//TEXTOS BIENVENIDA
 	public static final String BIENVENIDA = "Bienvenido a la interfaz de banca digital";
+	//TEXTOS MENUS
 	public static final String MENU_INICIO = "Menu de acciones iniciales: ";
 	public static final String[] MENU_INICIO_ACCIONES_USER= {"SALIR","Crear cuenta","Ingresar dinero a cuenta","Retirar dinero de una cuenta","Mover dinero entre cuentas"};
 	public static final String[] MENU_INICIO_ACCIONES_ADMIN= {"SALIR","Dar de alta a nuevo usuario","Consultar movimiento de un usuario","Cerrar cuenta de usuario"};
 	public static final String SELECIONA_ACCION = "SELECIONA UNA ACCION [Introduce el número de la acción a realizar] : ";
+	
+	//TEXTOS CUENTAS
+	public static final String INGRESO_DE_DINERO_EN_CUENTA = "No se puede hacer ingresos negatigos";
+	public static final String CUENTA_ERROR_GASTO_SUPERIOR_A_SALDO = "Se anulo el proceso, ya que el saldo es insuficiente";
+	public static final String CUENTA_ERROR_GASTO_NO_POSITIVO = "No se puede realizar el pago ya que no hay un saldo positivo";
+	public static final String CUENTA_ERROR_RETIRO_NO_POSITIVO = "No se puede realizar la retirada de dinero ya que no hay un saldo positivo";
+	public static final String CUENTA_ERROR_PASAR_NO_POSITIVO = "No se puede realizar el pase de dinero ya que no hay un saldo positivo";
+
+	
 
 }
