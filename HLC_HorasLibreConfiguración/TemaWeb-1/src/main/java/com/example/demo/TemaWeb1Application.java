55f4fd7786847e2a4a74dc84318f432e4b40b533
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2025-01-10 17:44:12 +0100

    [HLC]

---
+++ HLC_HorasLibreConfiguración/TemaWeb-1/src/main/java/com/example/demo/TemaWeb1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemaWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemaWeb1Application.class, args);
+	}
+
+}
