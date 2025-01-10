392128f6cc783b8913affbbf310a7239e17e4ad0
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2025-01-10 18:18:28 +0100

    [HLC]Ejercicio1

---
+++ HLC_HorasLibreConfiguración/TemaWeb-1/src/main/java/com/example/demo/AnuncioControlador.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AnuncioControlador {
+	
+	@GetMapping("/nuevoAnuncio")
+	public String nuevoAnuncio() {
+		return "nuevoAnuncio";
+	}
+	@GetMapping("/anuncio")
+	public String anuncio(@RequestParam(name="nombre", required=false, defaultValue="World") String nombre, Model model) {
+		model.addAttribute("nombre", nombre);
+		return "anuncio";
+	}
+
+}
