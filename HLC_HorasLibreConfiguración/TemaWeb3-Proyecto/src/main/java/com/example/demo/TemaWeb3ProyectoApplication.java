472d803ebed47267a24845680dac71b1fc0225bd
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2025-01-17 18:57:15 +0100

    [HLC]Spring boot

---
+++ HLC_HorasLibreConfiguración/TemaWeb3-Proyecto/src/main/java/com/example/demo/TemaWeb3ProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemaWeb3ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemaWeb3ProyectoApplication.class, args);
+	}
+
+}
