088eabeb1ed5b177d0d29a6bd998e00231a269b9
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2025-01-31 21:14:57 +0100

    [DEC] Ejercicio cuerpo

---
+++ DEC_DesarrolloEntornoCliente/09-AJAX/EJERCICIOS/07- cuerpo humano/pagina1.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$id = $_REQUEST['cod'];
+$conexion;
+try {
+    $dbname = "cuerpohumano";
+    $conexion = new PDO("mysql:host=localhost;dbname=" . $dbname . ";charset=utf8", "root", "");
+    // echo "Se ha establecido una conexión con el servidor de bases de datos.";
+    return $conexion;
+} catch (PDOException $e) {
+    echo "No se ha podido establecer conexión con el servidor de bases de datos.<br>";
+    die("Error: " . $e->getMessage());
+}
+$consulta = $conexion->query("SELECT * FROM cliente WHERE id = $id");
+$resultado_api = $consulta->fetchObject();
+
+$conexion = null;
+
+echo $resultado_api;
