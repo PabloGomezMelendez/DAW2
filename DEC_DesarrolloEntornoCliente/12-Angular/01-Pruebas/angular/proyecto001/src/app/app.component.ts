09537dce6a70e90ef7b96345325511e6b24a4cce
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2025-02-14 21:15:09 +0100

    [DEC] Parctica con Angula 01

--- DEC_DesarrolloEntornoCliente/12-Angular/01-Pruebas/angular/proyecto001/src/app/app.component.ts
+++ DEC_DesarrolloEntornoCliente/12-Angular/01-Pruebas/angular/proyecto001/src/app/app.component.ts
@@ -8,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto001';
+  nombre = 'Pablo';
+  edad = 25;
+  fumador = false;
+  estudiante = true;
+  sueldos: number[] = [1000, 2000, 3000];
+  provinciasAndaluzas = [{ id: 1, nombre: 'Almería' }, { id: 2, nombre: 'Cádiz' }, { id: 3, nombre: 'Córdoba' }, { id: 4, nombre: 'Granada' }, { id: 5, nombre: 'Huelva' }, { id: 6, nombre: 'Jaén' }, { id: 7, nombre: 'Málaga' }, { id: 8, nombre: 'Sevilla' }];
+
+  contador = 0;
+
+  esFumador() {
+    return this.fumador ? 'Es fumador' : 'No es fumador';
+  }
+  diHolaMundo() {
+    alert('!Hola Mundo¡');
+  }
+
+  incrementar() {
+    this.contador++;
+  }
+  decrementar() {
+    this.contador--;
+  }
+  reiniciar() {
+    this.contador = 0;
+  }
+
 }
