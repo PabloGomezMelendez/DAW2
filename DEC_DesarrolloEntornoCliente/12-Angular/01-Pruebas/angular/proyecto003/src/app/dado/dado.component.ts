5a3f83c7614e2502e316e64fdc5e2c5e7b612955
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2025-02-19 21:15:59 +0100

    [DEC] Proyecto003 - dado funcional - El dado muestar los valores random - TODO: estilos al dado bonito

--- DEC_DesarrolloEntornoCliente/12-Angular/01-Pruebas/angular/proyecto003/src/app/dado/dado.component.ts
+++ DEC_DesarrolloEntornoCliente/12-Angular/01-Pruebas/angular/proyecto003/src/app/dado/dado.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './dado.component.css'
 })
 export class DadoComponent {
+  valor: number = 0;
+  //Genera valores aleatroiso apara el dado
+  constructor() {
+    this.valor = this.generarValorAleatorio();
+  }
+  generarValorAleatorio(): number {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+
 
 }
