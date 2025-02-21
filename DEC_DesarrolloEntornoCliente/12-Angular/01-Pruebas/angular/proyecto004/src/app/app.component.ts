42a4ad964ac8b093441c919fd1420cbcb2f538b4
Pablo Gomez Melendez <pablo.gomez-melendez@iesruizgijon.com>
2025-02-21 19:30:58 +0100

    [DEC] Proyecto004 - Instalar Bootstrap como dependencia del proyecto usando el gestor de paquetes npm: Boostrap en el 4

--- DEC_DesarrolloEntornoCliente/12-Angular/01-Pruebas/angular/proyecto004/src/app/app.component.ts
+++ DEC_DesarrolloEntornoCliente/12-Angular/01-Pruebas/angular/proyecto004/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any; // ADD THIS
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,46 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'proyecto004';
+  ngOnInit(): void {
+    $('[data-bs-toggle="popover"]').popover();
+  }
+
+  valor1!: number;
+  valor2!: number;
+  valor3!: number;
+
+  intentos = 0;
+  contadorVictorias = 0;
+  media = 0.0;
+
+  resultado = "";
+
+  constructor() {
+    this.lanzarDados();
+  }
+
+  lanzarDados() {
+    this.valor1 = this.generarAleatorio();
+    this.valor2 = this.generarAleatorio();
+    this.valor3 = this.generarAleatorio();
+    this.comprobarResultado();
+  }
+
+  comprobarResultado() {
+    if (this.valor1 == this.valor2 && this.valor1 == this.valor3) {
+      this.resultado = "Has Ganado!";
+      this.contadorVictorias++;
+    } else {
+      this.resultado = "-_-";
+    }
+    this.intentos++;
+    this.media = this.contadorVictorias / this.intentos;
+  }
+
+  generarAleatorio() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
 }
--- DEC_DesarrolloEntornoCliente/12-Angular/01-Pruebas/angular/proyecto004/src/app/dado/dado.component.ts
+++ DEC_DesarrolloEntornoCliente/12-Angular/01-Pruebas/angular/proyecto004/src/app/dado/dado.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dado',
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './dado.component.css'
 })
 export class DadoComponent {
+
+  // Es necesario inportar el @Input
+  @Input() valor: number = 1;
 
 }
